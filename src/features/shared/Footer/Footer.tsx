7c78be2fe7f0bc471dab2d0e2dc9ca657ba67aa6
BluubiDev <bluubi.dev@gmail.com>
2023-11-21 13:57:03 +0100

    feat: create footer

---
+++ src/features/shared/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { Anchor } from "../Anchor/Anchor.tsx";
+import "./footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <Anchor link={"new/payment"} label={"Add payment"} />
+    </footer>
+  );
+};
