ad2e1555a2416450a54ba5a9373853b78c155007
BluubiDev <bluubi.dev@gmail.com>
2023-11-21 13:56:53 +0100

    refactor: added style to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
+import Svg from "./features/shared/Svg/Svg.tsx";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <h1> Welcome to Friendly App </h1>
+    <section className={"container"}>
+      <Svg src={"src/assets/imgs/Larry-sharing-payment.svg"} alt={""} />
       <Outlet />
-    </>
+    </section>
   );
 };
 
