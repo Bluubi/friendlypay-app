1fecfe6fe33289f7735ef6e7b5a6757f5a2d7481
BluubiDev <bluubi.dev@gmail.com>
2023-11-21 13:57:16 +0100

    feat: create SVG, #16

--- src/features/shared/Svg/Svg.tsx
+++ src/features/shared/Svg/Svg.tsx
@@ -1,12 +1,14 @@
-import {ComponentProps} from "react";
-import './svg.css';
+import { ComponentProps } from "react";
+import "./svg.css";
 
-type SvgProps = ComponentProps<'img'>;
+type SvgProps = ComponentProps<"img">;
 
-export const Svg = ({ src, alt}: SvgProps) => {
-    return (<div className={'svg'}>
-        <img src={src} alt={alt} />
-    </div>)
-}
+export const Svg = ({ src, alt }: SvgProps) => {
+  return (
+    <div className={"svg"}>
+      <img src={src} alt={alt} />
+    </div>
+  );
+};
 
 export default Svg;
