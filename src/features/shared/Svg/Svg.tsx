3409cd13f17c425c3fcf1c02f0b74c233d3c1327
BluubiDev <bluubi.dev@gmail.com>
2023-11-21 13:55:45 +0100

    feat: add and create new SVG tag. Add svgs to use #16

---
+++ src/features/shared/Svg/Svg.tsx
@@ -0,0 +1,12 @@
+import {ComponentProps} from "react";
+import './svg.css';
+
+type SvgProps = ComponentProps<'img'>;
+
+export const Svg = ({ src, alt}: SvgProps) => {
+    return (<div className={'svg'}>
+        <img src={src} alt={alt} />
+    </div>)
+}
+
+export default Svg;
