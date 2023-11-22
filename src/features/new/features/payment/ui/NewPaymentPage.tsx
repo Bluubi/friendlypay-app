04887d82fca6e0e9e28fa04c005b811f35ee3945
BluubiDev <bluubi.dev@gmail.com>
2023-11-22 12:15:39 +0100

    feat: move logic to page and rename file to fit with React practices. Remove unnecessary file

---
+++ src/features/new/features/payment/ui/NewPaymentPage.tsx
@@ -0,0 +1,36 @@
+import {Button} from "../../../../shared/Button/Button.tsx";
+import {ControlGroup, getControlInput} from "../../../../shared/Form/Control-Group.tsx";
+import {PAYMENT_CONTROLS} from "../domain/payment-controls.ts";
+import * as REACT from "react";
+import {container} from "tsyringe";
+import {NewPaymentRepository} from "../domain/new-payment-repository.ts";
+import './new-payment-page.css';
+
+export const NewPaymentPage = () => {
+
+    const newPayment = (event: REACT.FormEvent) => {
+        event.preventDefault();
+
+        const newPaymentRepository = container.resolve<NewPaymentRepository>("NewPaymentRepository");
+
+        const payment =  {
+            owner: getControlInput('[name="owner"]').value,
+            amount: Number(getControlInput('[name="amount"]').value),
+            activity: getControlInput('[name="activity"]').value,
+        };
+
+        newPaymentRepository.execute(payment);
+    };
+
+
+    return (
+    <div className={'bottom-sheet'}>
+      <form onSubmit={(event) => newPayment(event)}>
+        <ControlGroup controls={PAYMENT_CONTROLS}></ControlGroup>
+        <div>
+          <Button label={"Create payment"}> </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
