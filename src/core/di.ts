a4b9c05e095d750842fd3d3f952b22e5d9926c5e
BluubiDev <bluubi.dev@gmail.com>
2023-11-24 09:58:28 +0100

    chore: change name of di

--- src/core/di.ts
+++ src/core/di.ts
@@ -1,6 +1,6 @@
 import { container } from "tsyringe";
 import { NewPaymentRepository } from "../features/new/features/payment/domain/new-payment-repository.ts";
-import { NewPaymentCmdLocalhostRepository } from "../features/new/features/payment/infrastructure/new-payment-cmd-localhost-repository.ts";
+import { NewPaymentCmdLocalStorageRepository } from "../features/new/features/payment/infrastructure/new-payment-cmd-local-storage-repository.ts";
 container.register<NewPaymentRepository>("NewPaymentRepository", {
-  useClass: NewPaymentCmdLocalhostRepository,
+  useClass: NewPaymentCmdLocalStorageRepository,
 });
