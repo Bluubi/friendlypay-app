93b0ee6ba1c044bc2d0901c3562bdda488c66e85
BluubiDev <bluubi.dev@gmail.com>
2023-11-24 10:00:38 +0100

    feat: create core svg

---
+++ src/core/svg.ts
@@ -0,0 +1,9 @@
+import LARRY_WITH_FRIEND from "svg/Larry-sharing-with-friend.svg";
+import LARRY_SHARING_PAYMENT from "svg/Larry-sharing-payment.svg";
+
+const SVG = {
+  LARRY_WITH_FRIEND,
+  LARRY_SHARING_PAYMENT,
+};
+
+export default SVG;
