6040d2fb9fa462465dcaa6a528b1552357082400
BluubiDev <bluubi.dev@gmail.com>
2023-11-26 13:18:50 +0100

    chore: prettier,

--- src/shared/Svg/Svg.tsx
+++ src/shared/Svg/Svg.tsx
@@ -2,8 +2,8 @@ import { ComponentProps } from "react";
 import "./svg.css";
 
 type SvgProps = ComponentProps<"img"> & {
-    width?: string
-}
+  width?: string;
+};
 
 export const Svg = ({ src, alt, width }: SvgProps) => {
   return (
